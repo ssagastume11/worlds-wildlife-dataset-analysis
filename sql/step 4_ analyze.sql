e9c206fb043976a1bcb677fc0052b2ff7a938c94
ssagastume11 <sagassergio123@gmail.com>
2025-10-14 14:54:08 -0400

    BigQuery SQL queries

--- sql/step 4_ analyze.sql
+++ sql/step 4_ analyze.sql
@@ -1,1 +1,8 @@
+-- Objective:
+-- Analyze the cleaned data to identify meaningful trends, correlations, and patterns that reveal how agricultural productivity relates to wildlife population decline and biodiversity loss across regions.
 
+-- Key Actions:
+-- * Calculate average crop yiels by region and year to observe productivity trends.
+-- * Compare yield growth rates with species population changes.
+-- * Identify regions achieving high productivity while minimizing biodiversity loss.
+-- * Use correlation and ranking queries to highlight significant patterns and contrasts.

2b694d0cd7669286a10d3222f88fb97e47bb1bc2
ssagastume11 <sagassergio123@gmail.com>
2025-10-14 14:53:47 -0400

    BigQuery SQL queries

--- sql/step 3_ process.sql
+++ sql/step 3_ process.sql
@@ -1,1 +1,8 @@
+-- Objective:
+-- Transform and summarize the cleaned dataset to prepare it for detailed analysis. This step focuses on generating meaningful metrics that help identify trends in agricultural productivity and its potential relationship with wildlife decline.
 
+-- Key Actions:
+-- * Aggregate cleaned data by country and year to calculated average maize yield.
+-- * Compute year-over-year changes in agricultural productivity.
+-- * Classify regions based on whether their productivity is above or below the global median.
+-- * Prepare a structured dataset for analyzing correlations between agricultural output and biodiversity trends.

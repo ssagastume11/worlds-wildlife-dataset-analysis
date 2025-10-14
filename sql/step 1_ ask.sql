6e5a911ad2675d8ec1a6cc2f0de39bd6baa26171
ssagastume11 <sagassergio123@gmail.com>
2025-10-14 14:53:00 -0400

    BigQuery SQL queries

--- sql/step 1_ ask.sql
+++ sql/step 1_ ask.sql
@@ -1,1 +1,8 @@
+-- Business Task:
+-- The main goal of this project is to identify the relationship between agricultural land use, species population decline, and regional agricultural productivity. 
+-- This analysis aims to highlight how human-driven farming expansion affects biodiversity and how certain regions achieve higher productivity while maintaining wildlife balance.
 
+-- Key Questions:
+-- 1. How does agricultural land expansion correlate with wildlife population decline across regions?
+-- 2. Which regions have improved food productivity while reducing habitat loss?
+-- 3. What trends link crop yield improvements to biodiversity conservation worldwide?

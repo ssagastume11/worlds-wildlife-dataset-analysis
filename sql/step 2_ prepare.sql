64764331638f7d09aa617c7ea7b18f47fc546697
ssagastume11 <sagassergio123@gmail.com>
2025-10-14 14:53:20 -0400

    BigQuery SQL queries

--- sql/step 2_ prepare.sql
+++ sql/step 2_ prepare.sql
@@ -1,1 +1,8 @@
+-- Objective:
+-- The goal of this step is to load, clean, and organize the World's Wildlife dataset to ensure it is ready for accurate analysis.
+-- This includes validating data integrity, handling missing values, converting data types, and preparing it for correlation analysis between agricultural land use, crop yields, and wildlife indicators.
 
+-- Key Actions:
+-- * Ensuring correct data types.
+-- * Handling null or missing values.
+-- * Creating a cleaned version of the dataset.
